435ea5e5c1e5ef7d974900baae10bf0c8491d688
kavehsgh <kave.sgh@gmail.com>
2025-12-22 12:00:38 +0300

    Add reminder for Docker Engine to run in run_windows.bat

--- Implementation Code/run_windows.bat
+++ Implementation Code/run_windows.bat
@@ -2,6 +2,7 @@
 cd /d "%~dp0"
 echo Starting VINS-Mono Container...
 echo Ensure VcXsrv is running with "Disable access control" checked!
+echo Ensure Docker Engine is running!
 
 docker-compose up -d --build
 docker exec -it vins_mono_container bash
